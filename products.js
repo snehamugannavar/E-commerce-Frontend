b7dd75812bd26e0e0d8defbb33506e9b0392f09c
snehamugannavar <u02cs24s0099@klebcadwd.com>
2025-05-06 17:09:48 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,43 @@
+const products = [
+    {
+        id: 1,
+        title: "Wireless Headphones",
+        price: 2099,
+        description: "Noise Cancelling over-ear Bluetooth Headphones",
+        category: "Electronics",
+        image: "images/headphones.webp",
+    },
+    {
+        id: 2,
+        title: "Smart Watch",
+        price: 2499,
+        description: "Fitness Smart Watch with Heart Rate Monitor",
+        category: "Electronics",
+        image: "images/smart watch.webp",
+    },
+    {
+        id: 3,
+        title: "Women's Kurta",
+        price: 439,
+        description: "100% Cotton Printed T-shirt",
+        category: "Clothing",
+        image: "images/kurta.jpg",
+    },
+    {
+        id: 4,
+        title: "Mobiles",
+        price: 65499,
+        description: "64 GB RAM and 128 GB storage  ",
+        category: "Electronics",
+        image: "images/iphone.jpg",
+    },
+    {
+        id: 5,
+        title: "Laptop",
+        price: 67999,
+        description: "16 GB RAM and 512 GB SSD Memory Card ",
+        category: "Electronics",
+        image: "images/laptop.webp",
+    }
+    // Add more product as needed
+];
